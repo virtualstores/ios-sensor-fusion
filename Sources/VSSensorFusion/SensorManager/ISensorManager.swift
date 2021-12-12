1f8d01136e2838b1675476595298fbc2f3f26fb8
Karl Söderberg <karl.soderberg@shortcut.io>
2021-12-12 20:58:50 +0100

    Updated dockumentaion

--- Sources/VSSensorFusion/SensorManager/ISensorManager.swift
+++ Sources/VSSensorFusion/SensorManager/ISensorManager.swift
@@ -10,11 +10,19 @@ import Foundation
 import VSFoundation
 import Combine
 
+/// Manager for MotionSensor data. Will track device motions and publish them to sensorPublisher.
 public protocol ISensorManager {
+    
+    /// Is SensorManager running or not. Determined by start()  and stop()
     var isRunning: Bool { get }
+    
+    /// Publishes the latest sensor data. Update interval is determined by implementing class
     var sensorPublisher: CurrentValueSubject<MotionSensorData?, SensorError> { get }
     
+    /// Starts sensor managers sensor lokup. Will produce results to sensorPublisher.
     func start() throws
+    
+    /// Stops sensor managers sensor.
     func stop()
 }
 
